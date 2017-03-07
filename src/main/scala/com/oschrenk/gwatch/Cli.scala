09b03f8097230f5fe4d5f29743c18a41c79b3e44
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-03-07 21:03:07 +0100

    Print branches

--- src/main/scala/com/oschrenk/gwatch/Cli.scala
+++ src/main/scala/com/oschrenk/gwatch/Cli.scala
@@ -1,25 +1,35 @@
 package com.oschrenk.gwatch
 
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.Ref
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 
-import java.io.File;
+import java.io.File
+
 
 object Cli extends App {
+
+  import scala.collection.JavaConverters._
 
   val workingDir = new File(".").getAbsolutePath()
   val repoDir = new File(workingDir, ".git").getCanonicalFile()
 
-  val repository = new FileRepositoryBuilder().setGitDir(repoDir)
+  val repo = new FileRepositoryBuilder().setGitDir(repoDir)
     .readEnvironment() // scan environment GIT_* variables
     .findGitDir()      // scan up the file system tree
     .build()
+  val git = new Git(repo)
 
-  println("Having repository: " + repository.getDirectory());
+  println("Having repository: " + repo.getDirectory())
 
   // the Ref holds an ObjectId for any type of object (tree, commit, blob, tree)
-  val head = repository.exactRef("refs/heads/master");
-  println("Ref of refs/heads/master: " + head);
+  val head = repo.exactRef("refs/heads/master")
+  println("Ref of refs/heads/master: " + head)
+
+  val branches: Seq[Ref] = asScalaBuffer(git.branchList().call())
+  branches.foreach { ref =>
+    println(s"Branch: ${ref.getName()} ${ref.getObjectId().getName()}")
+  }
+
 }
